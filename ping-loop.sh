06be0b34f1bb09bf72325215008444ad7e238611
AshutoshSinghDhauni <31855786+AshutoshSinghDhauni@users.noreply.github.com>
2018-09-27 17:37:15 -0400

    Update ping-loop.sh

--- ping-loop.sh
+++ ping-loop.sh
@@ -2,5 +2,6 @@
 
 for ip in $(seq  210 220) ;do
 ping -c 1 192.168.43.$ip |  grep "bytes from" | cut -d " " -f4 | cut -d ":" -f1 &
+//&  is used to limit the process in background used for speeding the process 
 done
 
